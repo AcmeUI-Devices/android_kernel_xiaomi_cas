c065d6a1648d0e60c6dd9b550e18c841b7cedd59
tarsin <yuanqingxiang233@163.com>
2022-08-27 08:09:55 +0800

    zram: Make lz4 default

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -19,10 +19,10 @@
 #include "zcomp.h"
 
 static const char * const backends[] = {
+	"lz4",
+#if IS_ENABLED(CONFIG_CRYPTO_LZO)
 	"lzo",
 	"lzo-rle",
-#if IS_ENABLED(CONFIG_CRYPTO_LZ4)
-	"lz4",
 #endif
 #if IS_ENABLED(CONFIG_CRYPTO_LZ4HC)
 	"lz4hc",
