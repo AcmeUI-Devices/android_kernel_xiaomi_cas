4cf1d5883326a42ab7b13bc36d32f3041cc10997
tarsin <yuanqingxiang233@163.com>
2023-02-23 18:06:05 +0800

    zram: Add zstd to default algos

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -20,6 +20,7 @@
 
 static const char * const backends[] = {
 	"lz4",
+	"zstd",
 #if IS_ENABLED(CONFIG_CRYPTO_LZO)
 	"lzo",
 	"lzo-rle",
@@ -29,9 +30,6 @@ static const char * const backends[] = {
 #endif
 #if IS_ENABLED(CONFIG_CRYPTO_842)
 	"842",
-#endif
-#if IS_ENABLED(CONFIG_CRYPTO_ZSTD)
-	"zstd",
 #endif
 	NULL
 };
