torvalds/linux

f2fs: allocate trace path buffer from names_cache
It would be better to use the dedicated slab to store path.

Signed-off-by: Wu Bo <bo.wu@vivo.com>
Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

fs/f2fs/file.c
@@ -4372,7 +4372,7 @@ static void f2fs_trace_rw_file_path(struct kiocb *iocb, size_t count, int rw)
 	struct inode *inode = file_inode(iocb->ki_filp);
 	char *buf, *path;
 
-	buf = f2fs_kmalloc(F2FS_I_SB(inode), PATH_MAX, GFP_KERNEL);
+	buf = f2fs_getname(F2FS_I_SB(inode));
 	if (!buf)
 		return;
 	path = dentry_path_raw(file_dentry(iocb->ki_filp), buf, PATH_MAX);
@@ -4385,7 +4385,7 @@ static void f2fs_trace_rw_file_path(struct kiocb *iocb, size_t count, int rw)
 		trace_f2fs_dataread_start(inode, iocb->ki_pos, count,
 				current->pid, path, current->comm);
 free_buf:
-	kfree(buf);
+	f2fs_putname(buf);
 }
 
 static ssize_t f2fs_file_read_iter(struct kiocb *iocb, struct iov_iter *to)

AcmeUI-Devices/android_kernel_xiaomi_cas

fs/f2fs/file.c
@@ -4405,7 +4405,7 @@ static ssize_t f2fs_file_read_iter(struct kiocb *iocb, struct iov_iter *iter)
 		return -EOPNOTSUPP;
 
 	if (trace_f2fs_dataread_start_enabled()) {
-		char *p = f2fs_kmalloc(F2FS_I_SB(inode), PATH_MAX, GFP_KERNEL);
+		char *p = f2fs_getname(F2FS_I_SB(inode));
 		char *path;
 
 		if (!p)
@@ -4413,14 +4413,14 @@ static ssize_t f2fs_file_read_iter(struct kiocb *iocb, struct iov_iter *iter)
 
 		path = dentry_path_raw(file_dentry(iocb->ki_filp), p, PATH_MAX);
 		if (IS_ERR(path)) {
-			kfree(p);
+			f2fs_putname(p);
 			goto skip_read_trace;
 		}
 
 		trace_f2fs_dataread_start(inode, iocb->ki_pos,
 					iov_iter_count(iter),
 					current->pid, path, current->comm);
-		kfree(p);
+		f2fs_putname(p);
 	}
 skip_read_trace:
 	ret = generic_file_read_iter(iocb, iter);
@@ -4570,8 +4570,7 @@ static ssize_t f2fs_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 		ret = preallocated;
 	} else {
 		if (trace_f2fs_datawrite_start_enabled()) {
-			char *p = f2fs_kmalloc(F2FS_I_SB(inode),
-						PATH_MAX, GFP_KERNEL);
+			char *p = f2fs_getname(F2FS_I_SB(inode));
 			char *path;
 
 			if (!p)
@@ -4579,12 +4578,12 @@ static ssize_t f2fs_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 			path = dentry_path_raw(file_dentry(iocb->ki_filp),
 								p, PATH_MAX);
 			if (IS_ERR(path)) {
-				kfree(p);
+				f2fs_putname(p);
 				goto skip_write_trace;
 			}
 			trace_f2fs_datawrite_start(inode, orig_pos, orig_count,
 					current->pid, path, current->comm);
-			kfree(p);
+			f2fs_putname(p);
 		}
 skip_write_trace:
 		ret = __generic_file_write_iter(iocb, from);